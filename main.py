67eeb767240699fe1644e6461c15766e96bad940
Victor A Medina <vmedina1014@gmail.com>
2020-01-02 20:09:19 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
@@ -8,21 +7,21 @@ app = Flask(__name__)
 
 
 def get_fact():
-
-    response = requests.get("http://unkno.com")
-
+    response=requests.get("http://unkno.com")
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
-
     return facts[0].getText()
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
+    fact=get_fact()
+    url="https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    data={'input_text':fact}
+    response=requests.post(url, data=data, allow_redirects=False)
+    new_url=response.headers['Location']
+    return "<a href='{}'>{}</a>".format(new_url,new_url)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
